ff8db2ab1ae26f88a3028201e34a8c2085579967
Uzair <uzairhajra76330@gmail.com>
2021-07-21 12:34:03 +0530

    Added functionality to varoius state methods

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,33 @@
 import React, { Component } from "react";
+import TodoItem from "./TodoItem";
 import Item from "./TodoItem";
 export default class TodoList extends Component {
   render() {
+    const { items, clearList, handleDelete, handleEdit } = this.props;
     return (
-      <section>
-        <h2>TodoList</h2>
-        <Item></Item>
-      </section>
+      <ul className="list-group my-5">
+        <h3 className="text-capitalize text-center">To-Do List</h3>
+        {items.map(item => {
+          return (
+            <TodoItem
+              key={item.id}
+              title={item.title}
+              handleDelete={() => {
+                handleDelete(item.id);
+              }}
+              handleEdit={() => {
+                handleEdit(item.id);
+              }}
+            ></TodoItem>
+          );
+        })}
+        <button
+          className="btn btn-danger col-12 text-uppercase mt-5"
+          onClick={clearList}
+        >
+          Clear List
+        </button>
+      </ul>
     );
   }
 }
